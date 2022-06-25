6c787701512deec6d4f05ab3a7bb8417af16a58f
Lucas Cabral <lucas.9cabral41@gmail.com>
2022-06-24 21:51:32 -0300

    Features - listando todos os partcipantes da sala

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
 import dayjs from 'dayjs'
+import joi from 'joi'
 
 dotenv.config()
 console.log(dayjs().format('HH:mm:ss'))
 const app = express()
 app.use(cors())
+
 app.use(express.json())
 
 const mongoClient = new MongoClient(process.env.MONGO_URI)
@@ -17,18 +19,37 @@ mongoClient.connect().then(() => {
   db = mongoClient.db('BatePapoUolDB')
 })
 
-app.get('/participants', async (req, res) => {
-  // É FEITA PRIMEIRO UMA ANALISE PRA VER QUEM ESTA ENTRANDO NA SALA, PRA DEPOIS EXIBIR AS MENSAGENS
+const participanteSchema = joi.object({
+  name: joi.string().required()
+})
 
+app.get('/participants', async (req, res) => {
   const { user } = req.headers
 
-  res.send(user)
-})
-app.post('/participants', async (req, res) => {
-  if (req.body.name === '') {
+  const validou = participanteSchema.validate({ name: user })
+  if (validou.error) {
     res.sendStatus(422)
     return
   }
+  const todosParticipantes = await db.collection('participantes').find().toArray()
+  console.log(todosParticipantes)
+
+  res.send(todosParticipantes)
+})
+app.post('/participants', async (req, res) => {
+  const validou = participanteSchema.validate({ name: req.body.name })
+  if (validou.error) {
+    res.sendStatus(422)
+    return
+  }
+  const nomeExiste = await db
+    .collection('participantes')
+    .findOne({ name: req.body.name })
+  if (nomeExiste !== null) {
+    res.status(409).send('Este nome já está em uso')
+    return
+  }
+
   const userLogado = { ...req.body, lastStatus: Date.now() }
 
   try {
