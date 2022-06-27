f4e6320eab57d3a49bc1fd344e7208e812869e0b
Lucas Cabral <lucas.9cabral41@gmail.com>
2022-06-27 18:02:54 -0300

    Conclusão do Projeto com o Delete de mensagens.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import dotenv from 'dotenv'
 import dayjs from 'dayjs'
 import joi from 'joi'
@@ -8,7 +8,6 @@ import joi from 'joi'
 dotenv.config()
 const app = express()
 app.use(cors())
-
 app.use(express.json())
 
 const mongoClient = new MongoClient(process.env.MONGO_URI)
@@ -27,6 +26,29 @@ const mensagensSchema = joi.object({
   text: joi.string().required(),
   type: joi.string().required()
 })
+
+setInterval(async () => {
+  const mensagensSaida = {
+    from: '',
+    to: 'Todos',
+    text: 'Saiu da sala...',
+    type: 'status',
+    time: dayjs().format('HH:mm:ss')
+  }
+
+  const tempoIvalido = Date.now() - 10000
+  const participantesInvalidos = await db
+    .collection('participantes')
+    .find({ lastStatus: { $lt: tempoIvalido } })
+    .toArray()
+  participantesInvalidos.forEach(async participante => {
+    mensagensSaida.from = participante.name
+    await db.collection('mensagens').insertOne(mensagensSaida)
+  })
+  await db
+    .collection('participantes')
+    .deleteMany({ lastStatus: { $lt: tempoIvalido } })
+}, 15000)
 
 app.get('/participants', async (req, res) => {
   const { user } = req.headers
@@ -59,27 +81,36 @@ app.post('/participants', async (req, res) => {
     res.sendStatus(422)
     return
   }
-  const nomeExiste = await db
-    .collection('participantes')
-    .findOne({ name: req.body.name })
-  if (nomeExiste !== null) {
-    res.status(409).send('Este nome já está em uso')
-    return
-  }
-
-  const userLogado = { ...req.body, lastStatus: Date.now() }
 
   try {
+    const nomeExiste = await db
+      .collection('participantes')
+      .findOne({ name: req.body.name })
+    if (nomeExiste !== null) {
+      res.status(409).send('Este nome já está em uso')
+      return
+    }
+
+    const userLogado = { ...req.body, lastStatus: Date.now() }
+
     await db.collection('participantes').insertOne(userLogado)
+    const mensagensEntrada = {
+      from: userLogado.name,
+      to: 'Todos',
+      text: 'Entrou na sala...',
+      type: 'status',
+      time: dayjs().format('HH:mm:ss')
+    }
+    await db.collection('mensagens').insertOne(mensagensEntrada)
     res.sendStatus(201)
   } catch (error) {
     res.send(422)
   }
 })
 
-app.get('/mensagens', async (req, res) => {
-  //{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: `${dayjs().format('HH:mm:ss')`}
+app.get('/messages', async (req, res) => {
   const { user } = req.headers
+
   const numeroMensagens = parseInt(req.query.limit)
   if (!numeroMensagens || numeroMensagens === 0) {
     try {
@@ -105,7 +136,7 @@ app.get('/mensagens', async (req, res) => {
     }
   }
 })
-app.post('/mensagens', async (req, res) => {
+app.post('/messages', async (req, res) => {
   const userRemetente = req.headers.user
   const userExiste = await db
     .collection('participantes')
@@ -140,7 +171,54 @@ app.post('/mensagens', async (req, res) => {
     res.sendStatus(422)
   }
 })
+app.delete('/messages/:ID_DA_MENSAGEM', async (req, res) => {
+  const idMensagem = req.params.ID_DA_MENSAGEM
+  const { user } = req.headers
+  const validou = participanteSchema.validate({ name: user })
+  if (!validou) {
+    res.sendStatus(404)
+    return
+  }
+  try {
+    const mensagemExiste = await db
+      .collection('mensagens')
+      .findOne({ _id: ObjectId(idMensagem) })
+    if (!mensagemExiste) {
+      res.sendStatus(404)
+      return
+    }
+    const donoMensagem = await db
+      .collection('mensagens')
+      .findOne({ _id: ObjectId(idMensagem), from: user })
+    if (!donoMensagem) {
+      res.sendStatus(401)
+      return
+    }
+    await db.collection('mensagens').deleteOne({ _id: ObjectId(idMensagem) })
+  } catch (error) {}
+})
+app.post('/status', async (req, res) => {
+  const { user } = req.headers
 
-app.post('/status', async (req, res) => {})
+  try {
+    const userExiste = await db
+      .collection('participantes')
+      .findOne({ name: user })
+    if (!userExiste) {
+      res.sendStatus(404)
+      return
+    }
 
-app.listen(5000, console.log('Servidor funfando'))
+    await db.collection('participantes').updateOne(
+      {
+        _id: userExiste._id
+      },
+      { $set: { lastStatus: Date.now() } }
+    )
+    res.sendStatus(200)
+  } catch (error) {
+    res.sendStatus(422)
+  }
+})
+
+app.listen(5000)
